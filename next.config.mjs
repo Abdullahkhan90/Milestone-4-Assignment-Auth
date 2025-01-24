5b3fe5a515cd8d625b0e67d197f18805b926fdc1
Hafiz Abdullah <160678612+Abdullahkhan90@users.noreply.github.com>
2025-01-25 04:55:02 +0500

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ["cdn.dummyjson.com", "lh3.googleusercontent.com",
+      "avatars.githubusercontent.com",
+      "raw.githubusercontent.com"],
+  },
+};
+
+export default nextConfig;
